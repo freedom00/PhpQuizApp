1cdb805e697739ce2cde1232ca7af3ff21fbc6ca
Zhaosheng Liu <jonathan888999@163.com>
2020-03-28 00:11:58 -0400

    format the code.

--- FinalProject/index.php
+++ FinalProject/index.php
@@ -3,7 +3,7 @@
 use Monolog\Logger;
 
 require "inc/functions.inc.php";
-// start session to have access to SESSION superglobal
+// start session to have access to SESSION super global
 
 $sender['log_file_name'] = "logs/users.log";
 $sender['log_type'] = Logger::INFO;
