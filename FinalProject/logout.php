3a398c4921dfbfd4107f4ef2f7fab46352661f92
Zhaosheng Liu <jonathan888999@163.com>
2020-03-28 21:52:10 -0400

    Add login logout and register function.

--- FinalProject/logout.php
+++ FinalProject/logout.php
@@ -1,4 +1,7 @@
 <?php
+
+use Monolog\Logger;
+
 require "inc/functions.inc.php";
 // start session to have access to SESSION superglobal
 
@@ -6,12 +9,12 @@ $sender['log_file_name'] = "logs/users.log";
 $sender['log_type'] = Logger::INFO;
 pushLog($sender);
 
+//log INFO - user has logged out (addtional parameter: user's name)
+$log->info($_SESSION['name'] . " has logged out ");
+
 // remove all existing session data
 session_destroy();
 session_unset();
-//log INFO - user has logged out (addtional parameter: user's name)
-$log->info($userName . " has logged out ");
-// redirect
-header("Location: login.php");
 
-?>
+// redirect
+header("Location: index.php");
--- FinalProject/register.php
+++ FinalProject/register.php
@@ -1,11 +1,32 @@
 <?php
 
 // include our file of stuff we use often
+use Monolog\Logger;
+
 require "inc/functions.inc.php";
 
-//test github
+$sender['log_file_name'] = "logs/users.log";
+$sender['log_type'] = Logger::INFO;
+pushLog($sender);
 
-/*-- 
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isFieldEmpty($_POST['inputName']) || isFieldEmpty($_POST['inputEmail']) || isFieldEmpty($_POST['inputPassword']) || isFieldEmpty($_POST['inputConfirmPassword'])) {
+        $sender['errorMessage'] = "All field are required.";
+    } else if (!filter_var($_POST['inputEmail'], FILTER_VALIDATE_EMAIL)) {
+        $sender['errorMessage'] = "Email is not valid.";
+    } else if ($_POST['inputPassword'] != $_POST['inputConfirmPassword']) {
+        $sender['errorMessage'] = "Password must be the same.";
+    }
+    if ($sender['errorMessage'] == "") {
+        $sender['tableName'] = STUDENT;
+        $sender['row'] = array('stuName' => $_POST['inputName'], 'stuEmail' => $_POST['inputEmail'], 'stuPassword' => password_hash($_POST['inputPassword'], PASSWORD_DEFAULT));
+        insertUpdate($sender);
+        $log->info($_POST['inputName'] . " has registered ");
+        header("Location: login.php");
+    }
+}
+
+/*--
 	Return the template page which is located in the templates folder
 */
 $sender['file_name'] = "register.twig";
